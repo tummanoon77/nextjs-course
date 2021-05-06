e235a961a0890a3c8f3187bd530b088b49a41855
Tummanoon Sripechpong <nukgroup@yahoo.com>
2021-05-05 20:04:16 -0400

    set up 2

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -2,7 +2,9 @@ import { Fragment } from 'react' ;
 import { useRouter } from 'next/router';
 
 import { getEventById } from '../../dummy-data';
-import EventSummary from '../../components'
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
 
 
 function EventDetailPage() {
@@ -15,9 +17,18 @@ function EventDetailPage() {
         return <p>No event found</p>
     }
     return (
-        <div>
-            <h1>Event Detail</h1>
-        </div>
+        <Fragment>
+            <EventSummary title={event.title} />
+            <EventLogistics 
+            date={event.date} 
+            address={event.location} 
+            image={event.image} 
+            imageAlt={event.title}
+            />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </Fragment>
     );
 }
 
